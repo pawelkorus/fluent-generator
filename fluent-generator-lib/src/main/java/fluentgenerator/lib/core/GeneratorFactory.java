96b2465fcea5e5f62c0182c4a9d2fd6876a58ed7
Pawel Korus <pawelkorus@ymail.com>
2016-04-01 17:03:49 +0200

    removing SupplierGenerator factory method from JDKProxyGeneratorFactory.

--- fluent-generator-lib/src/main/java/fluentgenerator/lib/core/GeneratorFactory.java
+++ fluent-generator-lib/src/main/java/fluentgenerator/lib/core/GeneratorFactory.java
@@ -1,8 +1,7 @@
 package fluentgenerator.lib.core;
 
-/**
- * Created by plks on 2016-03-31.
- */
 public interface GeneratorFactory {
+
 	<T> T generatorInstance(Class<T> generatorClass);
+
 }
--- fluent-generator-lib/src/main/java/fluentgenerator/lib/core/JDKProxyGeneratorFactory.java
+++ fluent-generator-lib/src/main/java/fluentgenerator/lib/core/JDKProxyGeneratorFactory.java
@@ -1,10 +1,7 @@
 package fluentgenerator.lib.core;
 
-import fluentgenerator.lib.generator.SupplierGenerator;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
-import java.util.function.Supplier;
 
 /**
  * Creates generator interface implementations using Java Dynamic Proxies and {@link GeneratorProxy} as a backend
@@ -32,17 +29,6 @@ public class JDKProxyGeneratorFactory implements GeneratorFactory {
 			handler);
 
 		return (T) obj;
-	}
-
-	/**
-	 * Creates instance of {@link Generator} which is an adapter for {@link Supplier} interface.
-	 *
-	 * @param supplier instance implementing {@link Supplier}
-	 * @param <T>      type of object produced by given supplier object
-	 * @return new instance of type {@link Generator}
-	 */
-	public <T> Generator<T> supplierGenerator(Supplier<T> supplier) {
-		return new SupplierGenerator<>(supplier);
 	}
 
 }
